abe05334476cdefd83a48b9ce8208ac5e631d9fb
Ivan <ivan.bondarev@aggrosoft.de>
2025-03-04 09:09:28 +0100

    language files

---
+++ translations/de/extranet_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$sLangName  = "Deutsch";
+
+$aLang = array(
+    'charset' => 'ISO-8859-15',
+    'ACCOUNT_TICKETS' => 'Meine Tickets',
+    'ACCOUNT_LICENSES' => 'Meine Lizenzen',
+    'ACCOUNT_SUBSCRIPTION_CONTRACTS' => 'Meine Abonnementvertr&auml;ge'
+);
---
+++ translations/de/voucher_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$sLangName  = "Deutsch";
+
+$aLang = array(
+    'charset' => 'ISO-8859-15',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_SEND_TO' => 'Diesen Gutschein senden an:',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_SEND_TO_CUSTOMER' => 'An mich',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_SEND_TO_OTHER' => 'Direkt an jemand anderen',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_OTHER_NAME' => 'Empf&auml;nger Name',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_OTHER_EMAIL' => 'Empf&auml;nger Email-Adresse',
+    'PAGE_DETAILS_PERSPARAM_VOUCHER_OTHER_MESSAGE' => 'Nachricht an Empf&auml;nger',
+    'PAGE_CHECKOUT_BASKETCONTENTS_ARTNOMBER' => 'Art. Nr.:',
+    'PAGE_CHECKOUT_BASKETCONTENTS_PERSPARAM' => 'Details:',
+);
